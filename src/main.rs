f6ee0d6962d58a7d88b60d4a84bae34c871a3dec
Manish Kumar <manish.kumar@crimsoni.com>
2023-09-04 22:20:16 +0530

    Init commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Secret Number is : {secret_number}");
+    loop {
+        println!("Please input your guess.");
+
+        let mut guessed_number = String::new();
+        io::stdin()
+            .read_line(&mut guessed_number)
+            .expect("Failed to read lines");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {guess}");
+
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Equal => {
+                    println!("You Win!");
+                    break;
+                }
+            Ordering::Greater => println!("Too Big!"),
+        }
+    }
+}
